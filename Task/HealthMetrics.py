00986168a62cd1f9244cfa036336f6d1b37a74ac
ce.12352051@gmail.com <ce12352051@gmail.com>
2025-12-18 12:02:19 +0530

    selvakumar

--- Task/HealthMetrics.py
+++ Task/HealthMetrics.py
@@ -3,7 +3,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("HealthMetricsofPatients.csv")
-data = pd.DataFrame(data)
 
 pair_chart = sns.pairplot(data, hue="Risk_Level", palette='husl')
 
--- Task/store_sales.py
+++ Task/store_sales.py
@@ -3,7 +3,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("store_sales.csv")
-data = pd.DataFrame(data)
 
 pair_chart = sns.pairplot(data, hue="Category", palette='husl')
 
