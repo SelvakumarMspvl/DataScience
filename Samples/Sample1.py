ede577096f58849692212d4528da22a4c9910e4d
ce.12352051@gmail.com <ce12352051@gmail.com>
2026-01-02 09:17:23 +0530

    selvakumar

--- Samples/Sample1.py
+++ Samples/Sample1.py
@@ -1,10 +1,4 @@
-import  pandas as pd
-import numpy as np
-my_list = [1,2,3,4,5,6,7]
-# length = len(my_list)
-# list_df = pd.DataFrame(my_list)
-# sum  = list_df.sum()
-# avg = sum/ length
-# avg = float(avg.iloc[0])
-# print(avg)
-
+x = 10
+def sample():
+    print(x)
+sample()
